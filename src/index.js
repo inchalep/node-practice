511f2eace1a46bdea866eda52f5d9ba035d40dbc
idnyaneshwaraxioned <dnyaneshwari@axioned.com>
2025-11-28 23:49:23 +0530

    feat: added react project

--- src/index.js
+++ src/index.js
@@ -1,22 +1,17 @@
-const express = require("express");
-const cors = require("cors");
-const router = require('./routers');
-const app = express();
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-app.use(
-  cors({
-    origin: "*",
-  })
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
-app.use('/api',router);
-
-// app.use('/',(req,res)=>{
-//   res.status(200).json({
-//     name: "Dnyaneshwar Inchale"
-//   })
-// })
-
-app.listen(3000,()=>{
-  console.log('App is running on port:', 3000)
-})
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
--- src/routers/userRoutes.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-router.get('/list',(req,res)=>{
-  res.send('Fetched user list')
-})
-module.exports = router
